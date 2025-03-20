2b8fac2134397815757a99b2a345dcad9371c9b5
Sruthirangaraj03 <sruthir.22cse@kongu.edu>
2025-03-20 14:02:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@
     docker login -u sruthir22cse -p dckr_pat_WU0vH7MQDSfTXIsdNRhcvNiye3U
     docker tag test1 sruthir22cse/docker
     docker push sruthir22cse/docker
-    
+    docker-compose up -d
