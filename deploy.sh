020839d9795276f0ff849519bd511dd00d2f4cb9
Sruthirangaraj03 <sruthir.22cse@kongu.edu>
2025-03-20 13:34:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u sruthir22cse -p dckr_pat_WU0vH7MQDSfTXIsdNRhcvNiye3U
-    docker tag test sruthir22cse/dev
+    docker tag test1 sruthir22cse/dev
     docker push sruthir22cse/dev
     
